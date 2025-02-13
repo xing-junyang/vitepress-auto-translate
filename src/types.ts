3c6cb21190d7deb4b4197e009bdd346637be9d19
xingjunyang <xingjunyang@smail.nju.edu.cn>
2025-02-13 16:26:34 +0800

    feat: updated types

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,8 @@ export interface TranslationSegment {
     type: 'text' | 'code' | 'math' | 'frontmatter' | 'html';
     content: string;
     needsTranslation: boolean;
-    tokenIndex?: number;
+    tokenIndex: [number, number];
+    length?: number;  // only for text segments
 }
 
 export interface FileInfo {
